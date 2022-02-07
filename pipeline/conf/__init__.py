dcd10ab14535a273d05bdc1287eecc58d2ea673d
Ayaee <afabre@artfx.fr>
2022-02-07 16:42:51 +0100

    day1

---
+++ pipeline/conf/__init__.py
@@ -0,0 +1,8 @@
+asset_path = "G:/Artfx/TD4/WS_MicroFilm/MOVIE/ASSETS/{name}/{task}/{state}/{name}_{task}_{version}.{extension}"
+
+modeling_path = "G:/Artfx/TD4/Pipeline_Git/pipeline/maya/templates/modeling_template.ma"
+
+
+path_test = "G:/Artfx/TD4/WS_MicroFilm/MOVIE/ASSETS"
+
+modeling_path_final = "G:/Artfx/TD4/WS_MicroFilm/MOVIE/ASSETS/CHAIR/MODELING/WIP/CHAIR_MODELING_V001.MA/modeling_template.ma"
