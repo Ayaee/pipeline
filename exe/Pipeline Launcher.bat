ccb38f5d823090455c2d00465ffc77103b9f2781
Ayaee <afabre@artfx.fr>
2022-05-30 17:16:39 +0200

    add folder rez and launcher
    ajout des dossiers rez et du launcher pour avoir l'intégralités des besoin demandé

---
+++ exe/Pipeline Launcher.bat
@@ -0,0 +1,3 @@
+rez env pipeline -- python -c \"from pipeline.ui import tool_window; tool_window.open()\"
+@pause
+
---
+++ rez/pipeline/0.0.1/package.py
@@ -0,0 +1,13 @@
+name = "pipeline"
+
+version = "0.0.1"
+
+description = "My pipeline Maya"
+
+requires = ['Qt.py']
+
+
+tools = []
+
+def commands():
+    env.PYTHONPATH.set("D:/ABYGAELLE_FABRE/Pipeline_Git")
